1c81c0571977282c4fe442ee93551f4beee8a26e
Tcauley91 <caleytyler@gmail.com>
2020-01-02 18:07:52 -0500

    updating portfolio

---
+++ myportfolio/js/init.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+// view application preview large
+const newLocal = $(document).ready(function () {
+  $('.materialboxed').materialbox();
+});
+
+// Scrollspy
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
+
